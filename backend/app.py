9470395c0e14147ddd2e8ce41add6d5d967513a5
Manju180324 <manjunathsrinivas2005@gmail.com>
2025-07-03 20:17:45 +0530

    fix: removed embedded git repo and re-added frontend correctly

---
+++ backend/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from db.config import db
+from routes.auth import auth_bp
+from flask_jwt_extended import JWTManager
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+CORS(app)
+
+#config
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecokart.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = 'your-256-bit-secret-key'
+
+#initialize extensions
+db.init_app(app)
+jwt = JWTManager(app)
+
+#register blueprints
+app.register_blueprint(auth_bp)
+
+#create tables
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def home():
+    return jsonify({"message": "WELCOME TO ECOKART "})
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ backend/routes/auth.py
@@ -0,0 +1,33 @@
+from flask import Blueprint, request, jsonify
+from db.config import db
+from models.user import User
+from flask_jwt_extended import create_access_token, create_refresh_token, jwt_required, get_jwt_identity
+from werkzeug.security import generate_password_hash, check_password_hash
+
+auth_bp = Blueprint('auth', __name__)
+
+@auth_bp.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    name = data.get('name')
+    email = data.get('email')
+    password = generate_password_hash(data.get('password'))
+    
+    user = User(name=name, email=email, password=password)
+    db.session.add(user)
+    db.session.commit()
+    
+    return jsonify({"message": "User registered successfully"}), 400
+    
+@auth_bp.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    email = data.get('email')
+    password = data.get('password')
+    
+    user = User.query.filter_by(email=email).first()
+    if not user or not check_password_hash(user.password, password):
+        return jsonify({"message": "Invalid credentials"}), 401
+    
+    token = create_access_token(identity=user.id)
+    return jsonify({"token": token,'user':{'id':user.id,'name':user.name}}), 200
